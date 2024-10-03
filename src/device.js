9a621b60b0d17e84da696652752697784b47288c
ウェブ完理 <webfullsympathy@ymail.ne.jp>
2024-10-03 19:42:10 +0900

    バグ修正

--- src/device.js
+++ src/device.js
@@ -1,10 +1,12 @@
 if (screen.availWidth < screen.availHeight){
-    let url = new URL(location.href)
-    let move_data =url.searchParams.get("text")
-    if (location.href.includes("?")){
-        location.href = location.href.substring(0, location.href.indexOf("?")) + "/m" + move_data
+    const url = new URL(location.href)
+    const move_data =url.searchParams.get("text")
+    if (location.href.includes("/m")){
+    }else if (location.href.includes("?")){
+        location.href = location.href.substring(0, location.href.indexOf("?")) + "/m?text=" + move_data
     }else if (location.href.charAt(location.href.length -1) == "/"){
         location.href = location.href.slice(0,-1) + "/m/"
-    }
+    }else{
     location.href = location.href = location.href + "/m/"
+    }
 }
