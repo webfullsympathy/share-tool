7a83acf6ab44f69ed4b31e0065496958cca02744
ウェブ完理 <webfullsympathy@ymail.ne.jp>
2024-10-05 07:41:45 +0900

    埋め込みコードのページも

--- src/device.js
+++ src/device.js
@@ -5,7 +5,7 @@ if (screen.availWidth < screen.availHeight){
     }else if (location.href.includes("?")){
         location.href = location.href.substring(0, location.href.indexOf("?")).replace("index.html","") + "/m?text=" + move_data
     }else if (location.href.charAt(location.href.length -1) == "/"){
-        location.href = location.href.slice(0,-1).replace("index.html","") + "/m/index.html"
+        location.href = location.href.slice(0,-1).replace("index.html/","") + "/m/index.html"
     }else{
     location.href = location.href.replace("index.html","") + "/m/index.html"
     }
