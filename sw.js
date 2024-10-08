68ed61d68209c5c18c9b8ea3089ad7d5458af953
ウェブ完理 <webfullsympathy@ymail.ne.jp>
2024-10-08 16:38:01 +0900

    js直し

--- sw.js
+++ sw.js
@@ -1,35 +1,8 @@
-var CACHE_NAME = 'pwa-sample-caches'
-var urlsToCache = [
-  // キャッシュ化したいコンテンツ
-]
+var CACHE_NAME = "share-tool-pwa"
+var urlsToCache = []
 
-self.addEventListener('install', function(event) {
-    console.log('sw event: install called')
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            return cache.addAll(urlsToCache)
-        })
-    )
-})
+self.addEventListener("install", function() {})
 
-self.addEventListener('fetch', function(event) {
-    console.log('sw event: fetch called')
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            return response ? response : fetch(event.request)
-        })
-    )
-})
+self.addEventListener("fetch", function() {})
 
-self.addEventListener('push', function(event){
-    console.log('sw event: push called')
-    var notificationDataObj = event.data.json()
-    var content = {
-        body: notificationDataObj.body
-    }
-    event.waitUntil(
-        self.registration.showNotification(notificationDataObj.title, content)
-    )
-})
+self.addEventListener("push", function(){})
