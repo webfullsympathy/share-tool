7293db5ab7138ea700b5ea813e2ea93f494e2c2f
ウェブ完理 <webfullsympathy@ymail.ne.jp>
2024-10-12 12:51:43 +0900

    直し3

---
+++ install/script.js
@@ -0,0 +1,22 @@
+var installPromptEvent
+
+
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault()
+    installPromptEvent = event
+    document.getElementById("install").disabled = false
+})
+function install(){
+    document.getElementById("install").disabled = true
+    installPromptEvent.prompt()
+    installPromptEvent.userChoice.then((install_result) => {
+        if(install_result.outcome === "accepted"){
+            console.log("インストールできた")
+        }else{
+            console.log("インストールできなかった")
+        }
+        installPromptEvent = null
+    })
+}
+
+install()
