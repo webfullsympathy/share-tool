20c7bd139be1f1e782ec606ef4d11e833a8ecb7d
ウェブ完理 <webfullsympathy@ymail.ne.jp>
2024-11-17 18:40:05 +0900

    https確認を廃止

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
-if(location.protocol === "https:"){
-    if(location.href.substring(0, person.indexOf("/")) == "https://share-tool.vercel.app" || location.href.substring(0, person.indexOf("?")) == "https://share-tool.vercel.app"){
-        // 共有ツールメインページの処理(https)
-        void(0)
-    }
-}else{
-    if(location.href.substring(0, person.indexOf("/")) == "http://share-tool.vercel.app" || location.href.substring(0, person.indexOf("?")) == "http://share-tool.vercel.app"){
-        if(confirm("HTTP通信です。HTTPS通信にしますか？") === true){
-            location.replace(location.href.replace("http","https"))
-        }else{
-            // 共有ツールメインページの処理(http)
-            void(0)
-        }
-    }
-}
+// 共有ツールで追加JSを記述
