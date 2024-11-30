66c0a497034a8274ae49b36d9e8edf09e969f9d4
ウェブ完理 <webfullsympathy@ymail.ne.jp>
2024-11-30 12:18:27 +0900

    エンコードかけるように

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function submit(){
     const service = get_1.options[get_2].innerText;
     async function other (){
         try {
-            await navigator.share({text: document.main.text.value});
+            await navigator.share({text: encodeURIComponent(document.main.text.value)});
         } catch (error) {
             console.error(error);
         }
@@ -30,24 +30,24 @@ function submit(){
         console.log("インスタンスのドメイン・IPアドレスを要求中")
         var instance = prompt("インスタンスの ドメイン・IPアドレス を入力してください。\n例：misskey.ioやmastodon.socialなど");
         console.log("入力されました")
-        var instance = "https://" + instance + "/share?text=" + document.main.text.value
+        var instance = "https://" + instance + "/share?text=" + encodeURIComponent(document.main.text.value)
         window.open(instance)
         console.log("インスタンスの共有ページを新しいタブで開きました")
     }else if(service == "Threads"){
         console.log("Threadsの処理を実行します")
-        window.open("https://threads.net/intent/post?text=" + document.main.text.value)
+        window.open("https://threads.net/intent/post?text=" + encodeURIComponent(document.main.text.value))
         console.log("Threadsの共有ページを新しいタブで開きました")
     }else if(service == "BlueSky"){
         console.log("BlueSkyの処理を実行します")
-        window.open("https://bsky.app/intent/compose?text=" + document.main.text.value)
+        window.open("https://bsky.app/intent/compose?text=" + encodeURIComponent(document.main.text.value))
         console.log("BlueSkyの共有ページを新しいタブで開きました")
     }else if(service == "X(旧Twitter)"){
         console.log("X(旧Twitter)の処理を実行します")
-        window.open("https://x.com/intent/post?text=" + document.main.text.value)
+        window.open("https://x.com/intent/post?text=" + encodeURIComponent(document.main.text.value))
         console.log("X(旧Twitter)の共有ページを新しいタブで開きました")
     }else if(service == "タイッツー"){
         console.log("タイッツーの処理を実行します")
-        window.open("https://taittsuu.com/share?text=" + document.main.text.value)
+        window.open("https://taittsuu.com/share?text=" + encodeURIComponent(document.main.text.value))
         console.log("タイッツーの共有ページを新しいタブで開きました")
     }else if(service == "LINE"){
         console.log("LINEの処理を実行します")
@@ -55,11 +55,11 @@ function submit(){
         console.log("LINEの共有ページを新しいタブで開きました")
     }else if(service == "Facebook"){
         console.log("Facebookの処理を実行します")
-        window.open("https://www.facebook.com/sharer.php?u=" + document.main.text.value)
+        window.open("https://www.facebook.com/sharer.php?u=" + encodeURIComponent(document.main.text.value))
         console.log("Facebookの共有ページを新しいタブで開きました")
     }else if(service == "WhatsApp"){
         console.log("WhatsAppの処理を実行します")
-        window.open("https://api.whatsapp.com/send/?text=" + document.main.text.value)
+        window.open("https://api.whatsapp.com/send/?text=" + encodeURIComponent(document.main.text.value))
         console.log("WhatsAppの共有ページを新しいタブで開きました")
     }else if(service == "その他"){
         console.log("その他の処理を実行します")
@@ -77,7 +77,7 @@ function submit(){
         console.log("コピーを試みました")
     }else if(service == "Linkedin"){
         console.log("Linkedinの処理を実行します")
-        window.open("https://www.linkedin.com/shareArticle?source=" + document.main.text.value)
+        window.open("https://www.linkedin.com/shareArticle?source=" + encodeURIComponent(document.main.text.value))
         console.log("Linkedinの共有ページを新しいタブで開きました")
     }
 }
